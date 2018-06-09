3289dbfda6beca1a0ac016d45583941c1961a7c1
Thomas Noelcke <noelcket@oregonstate.edu>
2018-06-09 16:47:02 -0700

    Finished adding jokes and adjusting schema

--- src/db/dbInit.sql
+++ src/db/dbInit.sql
@@ -21,10 +21,16 @@ DROP TABLE IF EXISTS `jokes`;
 CREATE TABLE `jokes` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `joke` VARCHAR(255) NOT NULL,
+    `catId` INT NOT NULL,
     `dadRating` INT NOT NULL,
     `userId` INT NOT NULL,
     FOREIGN KEY(`userId`) REFERENCES `users`(`id`), 
     `keywords` VARCHAR(255) NOT NULL
+    ON DELETE CASCADE,
+    FOREIGN KEY(`catId`) REFERENCES `categories`(`id`), 
+    `keywords` VARCHAR(255) NOT NULL
+    ON DELETE CASCADE
+    
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `comment`;
@@ -35,8 +41,10 @@ CREATE TABLE `comment` (
     `jokeId` INT NOT NULL,
     `userId` INT NOT NULL,
     `dadRating` INT NOT NULL,
-    FOREIGN KEY(`jokeId`) REFERENCES `jokes`(`id`), 
+    FOREIGN KEY(`jokeId`) REFERENCES `jokes`(`id`)
+    ON DELETE CASCADE, 
     FOREIGN KEY(`userId`) REFERENCES `users`(`id`)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 LOCK TABLES `users` WRITE;
@@ -54,15 +62,15 @@ INSERT INTO `categories` VALUES (5, "Knock Knock");
 UNLOCK TABLES;
 
 LOCK TABLES `jokes` WRITE;
-INSERT INTO `jokes` VALUES (1, "Really good joke", 2, 1, 0, "good, joke");
-INSERT INTO `jokes` VALUES (2, "I've got a really good UDP joke and i don't care if you get it.", 2, 2);
-INSERT INTO `jokes` VALUES (3, "I was going to go camping but it was too intents.", 4, 3, "udp, networking");
-INSERT INTO `jokes` VALUES (4, "Why don't they play poker in the jungle? To many Cheetas.", 5, 2, "jungle, cheeta, poker");
-INSERT INTO `jokes` VALUES (5, "What daes a zombie vegitarian ead? Graaaaaains.", 3, 1, 0, "zombie, vegitarian");
-INSERT INTO `jokes` VALUES (6, "My new thesaurus is terrible. Not only that, but its also terrible.", 5, , 0, "thesaurus, terrible");
-
-
-
-
-
+INSERT INTO `jokes` VALUES (1, "Really good joke", 4, 2, 1, "good, joke");
+INSERT INTO `jokes` VALUES (2, "I've got a really good UDP joke and i don't care if you get it.", 2, 2, 2);
+INSERT INTO `jokes` VALUES (3, "I was going to go camping but it was too intents.", 3, 4, 3, "udp, networking");
+INSERT INTO `jokes` VALUES (4, "Why don't they play poker in the jungle? To many Cheetas.", 3, 5, 3, "jungle, cheeta, poker");
+INSERT INTO `jokes` VALUES (5, "What daes a zombie vegitarian ead? Graaaaaains.", 3, 3, 3, "zombie, vegitarian");
+INSERT INTO `jokes` VALUES (6, "My new thesaurus is terrible. Not only that, but its also terrible.", 3, 5, 3, "thesaurus, terrible");
+INSERT INTO `jokes` VALUES (7, "My girlfriend thought I'd never be able to make a car out of spaghetti, You should have seen her face when i drove pasta!", 3, 5, 3, "car, pasta");
+INSERT INTO `jokes` VALUES (8, "What do you call the wife of a hippi? A Mississippi", 3, 4, 3, "hippi");
+INSERT INTO `jokes` VALUES (9, "What is a programmer's favorit hang out place? Foo Bar", 2, 2, 2, "foo, bar, programmer");
+INSERT INTO `jokes` VALUES (10, "Knock Knock, Who's There, Interupting Cow, Interupting Cow.....MOOOOOOOOOOOOO!", 5, 1, 1, "cow, interrupting");
+INSERT INTO `jokes` VALUES (11, "What did the cat say when he lost all his moeny? I'm Paw!", 1, 3, 1, "poor, lost, money, cheeta");
 UNLOCK TABLES;
